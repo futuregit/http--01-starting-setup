7af1aaa64dd2ead6f53582cec77afd378591c5ae
futuregit <gmail.com>
2019-05-30 22:10:18 -0400

    Added more http requests

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -10,10 +10,15 @@ class FullPost extends Component {
       if (this.props.id) {
         if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
           axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
-            .then(res => this.setState({loadedPost: res.data}))
+            .then(res => this.setState({loadedPost: res.data}));
         }
       }
 
+    }
+
+    deletePostHandler = () => {
+      axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+        .then(response => console.log(response));
     }
 
     render () {
@@ -27,7 +32,7 @@ class FullPost extends Component {
                   <h1>{this.state.loadedPost.title}</h1>
                   <p>{this.state.loadedPost.body}</p>
                   <div className="Edit">
-                      <button className="Delete">Delete</button>
+                      <button onClick={this.deletePostHandler} className="Delete">Delete</button>
                   </div>
               </div>
           );
