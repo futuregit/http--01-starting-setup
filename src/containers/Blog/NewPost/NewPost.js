8c7a74dedafea2062f25151aac5e9f445052fc00
futuregit <gmail.com>
2019-06-15 01:38:37 -0400

    Used history object for url redirection

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -23,8 +23,9 @@ class NewPost extends Component {
       };
       axios.post('/posts', data)
         .then(response => {
-          console.log(response)
-          this.setState({submitted: true});
+          console.log(response);
+          this.props.history.replace('/posts');
+          //this.setState({submitted: true});
         });
     }
 
