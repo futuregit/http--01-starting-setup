b750016052553abc36bf052db9e630e390b4ed64
futuregit <gmail.com>
2019-06-15 10:17:00 -0400

    Added lazy loading component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,11 +3,15 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+//import NewPost from './NewPost/NewPost';
+const AsyncNewPost = asyncComponent(()=> {
+  return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
   state = {
-    auth: false
+    auth: true
   }
     render () {
         return (
@@ -34,9 +38,10 @@ class Blog extends Component {
                 {/*/ <Route path="/" exact render={() => <h1>Home</h1>} />*/}
 
                 <Switch>
-                  {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+                  {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
                   <Route path="/posts"  component={Posts}/>
-                  <Redirect from="/" to="/posts" />
+                  <Route render={() => <h1 style={{textAlign:'center'}}> Not found </h1>} />
+                  {/*<Redirect from="/" to="/posts" />*/}
                   {/* <Route path="/"  component={Posts}/>*/}
                 </Switch>
             </div>
